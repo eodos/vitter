f659768348aaabfad215b9d6b51b676db97dd878
David Paul <eodos@eodos.net>
2014-09-14 00:04:25 +0200

    Secure passwords added to users, bcrypt gem

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "David Paul", email: "eodos0@gmail.com")
+    @user = User.new(name: "David Paul", email: "eodos0@gmail.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   test "user should be valid" do
@@ -48,5 +49,10 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase # this prevents case-insensitive
     assert_not duplicate_user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a"*5
+    assert_not @user.valid?
   end
 end
