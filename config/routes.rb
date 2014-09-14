47eabdad5fa4c62e15f867eddb01e390d10ee8cd
David Paul <eodos@eodos.net>
2014-09-14 20:14:00 +0200

    Sessions controller Login view Incorrect login flash message Login tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root             'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'signup'  => 'users#new'
+  get 'login'   => 'sessions#new'
+  get 'logout'  => 'sessions#destroy'
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
