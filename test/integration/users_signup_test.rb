aa1a283f2641aa7d5654398154f26cdc1a5c806c
David Paul <eodos@eodos.net>
2014-09-14 16:36:26 +0200

    Fixed gravatar size Added new user tests

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,5 +11,22 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
+    assert_select 'div.alert-danger'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name:  "David Paul",
+                                            email: "eodos0@gmail.com",
+                                            password:              "foobar",
+                                            password_confirmation: "foobar" }
+    end
+    assert_template 'users/show'
+    assert_not flash.nil?
+    assert_select 'div.alert'
+    assert_select 'div.alert-success'
   end
 end
