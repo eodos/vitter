c2f6e8c9e681d85d33ab43bd7bb933612d68057f
David Paul <eodos0@gmail.com>
2015-11-23 21:20:58 +0000

    fixed tests: css selectors

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,9 +5,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=#{root_path}]"
-    assert_select "a[href=#{help_path}]"
-    assert_select "a[href=#{about_path}]"
-    assert_select "a[href=#{signup_path}]"
+    assert_select "a[href=?]", root_path
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", signup_path
   end
 end
